8115e1590de049c223a7b629828cfa9354d8856e
valentijnscholten <valentijnscholten@gmail.com>
2022-04-18 10:05:51 +0200

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -18,7 +18,7 @@ fi
 # cat docker/entrypoint-integration-tests.sh
 # cat entrypoint-integration-tests.sh
 
-git config --global --add safe.directory /github/workspace
+git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
 echo "current commit"
 git log -1
